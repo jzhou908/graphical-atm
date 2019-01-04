de14338dc4ae7037b5dab759cfe9e0f68e06323b
jzhou908 <43217641+jzhou908@users.noreply.github.com>
2019-01-04 11:51:33 -0500

    Update CreateView.java

--- CreateView.java
+++ CreateView.java
@@ -1,28 +1,32 @@
 package view;
 
+import java.text.ParseException;
+import javax.swing.text.MaskFormatter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-
 import javax.swing.*;
+import model.BankAccount;
+import model.User;
 
 import controller.ViewManager;
 
 @SuppressWarnings("serial")
 public class CreateView extends JPanel implements ActionListener {
 	
+	private JButton quit;
 	private ViewManager manager;		// manages interactions between the views, model, and database
 	private JButton submit;
 	private JTextField firstname;
 	private JTextField lastname;
 	private JComboBox month;
 	private JComboBox day;
-	private JTextField year;
-	private JTextField phone;
+	private JFormattedTextField year;
+	private JFormattedTextField phone;
 	private JTextField street;
-	private JTextField zip;
-	private JTextField pin;
+	private JFormattedTextField zip;
+	private JFormattedTextField pin;
 	private JComboBox state;
 	private JTextField city;
 	/**
@@ -74,28 +78,60 @@ public class CreateView extends JPanel implements ActionListener {
 		day = new JComboBox(days);
 		day.setBounds(200, 55, 110, 35);
 		this.add(day);
-		year = new JTextField("Year");
+		try {
+			MaskFormatter phoneFormat = new MaskFormatter("####");
+			phoneFormat.setPlaceholderCharacter('_');
+			year = new JFormattedTextField(phoneFormat);
+		} catch (ParseException e) {
+			year.setText("");
+		}
 		year.setBounds(320, 55, 110, 35);
 		this.add(year);
-		phone = new JTextField("Phone Number");
+		try {
+			MaskFormatter phoneFormat = new MaskFormatter("(###) ###-####");
+			phoneFormat.setPlaceholderCharacter('_');
+			phone = new JFormattedTextField(phoneFormat);
+		} catch (ParseException e) {
+			phone.setText("");
+		}
 		phone.setBounds(180, 100, 170, 35);
 		this.add(phone);
 		street = new JTextField("Street Address");
-		//street.setBounds(205, 180, 200, 35);
+		street.setBounds(180, 145, 110, 35);
 		this.add(street);
-		zip = new JTextField("Zip Code");
-		//zip.setBounds(205, 180, 200, 35);
+		try {
+			MaskFormatter phoneFormat = new MaskFormatter("#####");
+			phoneFormat.setPlaceholderCharacter('_');
+			zip = new JFormattedTextField(phoneFormat);
+		} catch (ParseException e) {
+			zip.setText("");
+		}
+		zip.setBounds(180, 190, 60, 35);
 		this.add(zip);
-		pin = new JTextField("Input a 4 digit pin");
-		//pin.setBounds(205, 180, 200, 35);
+		try {
+			MaskFormatter phoneFormat = new MaskFormatter("####");
+			phoneFormat.setPlaceholderCharacter('_');
+			pin = new JFormattedTextField(phoneFormat);
+		} catch (ParseException e) {
+			pin.setText("");
+		}
+		pin.setBounds(180, 235, 60, 35);
 		this.add(pin);
 		city = new JTextField("City");
-		//city.setBounds(205, 180, 200, 35);
+		city.setBounds(180, 280, 100, 35);
 		this.add(city);
 		String[] states = {"AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"};
 		state = new JComboBox(states);
-		//state.setBounds(205, 180, 200, 35);
+		state.setBounds(180, 325, 50, 35);
 		this.add(state);
+		quit = new JButton("Quit");
+		quit.setBounds(100, 370, 100, 35);
+		quit.addActionListener(this);
+		this.add(quit);
+		submit = new JButton("Submit");
+		submit.setBounds(220, 370, 100, 35);
+		submit.addActionListener(this);
+		this.add(submit);
 	}
 	
 	/*
@@ -119,7 +155,17 @@ public class CreateView extends JPanel implements ActionListener {
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+		Object source = e.getSource();
+		if(source.equals(quit)) {
+			manager.switchTo(ATM.LOGIN_VIEW);
+		}
+		if(source.equals(submit)) { 
+			int dob = 
+			long phonenumber = Long.parseLong(String.valueOf(phone.getText().substring(1, 4) + ))
+			User user = new User(Integer.parseInt(String.valueOf(pin)), dob, phonenumber , firstname.getText(), lastname.getText(), street.getText(), city.getText(),(String) state.getSelectedItem(), zip.getText());
+			
+			manager.switchTo(ATM.HOME_VIEW);
+		}
 		// TODO
 		//
 		// this is where you'll setup your action listener, which is responsible for

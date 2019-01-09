856df978049501163ebfa79aae536b11a8bb0c04
jzhou908 <43217641+jzhou908@users.noreply.github.com>
2019-01-09 14:31:56 -0500

    Create ViewManager.java

---
+++ ViewManager.java
@@ -0,0 +1,104 @@
+package controller;
+
+import java.awt.CardLayout;
+import java.awt.Container;
+
+import javax.swing.JOptionPane;
+
+import data.Database;
+import model.BankAccount;
+import model.User;
+import view.ATM;
+import view.LoginView;
+import java.awt.Container;
+
+import javax.swing.JOptionPane;
+
+import data.Database;
+import model.BankAccount;
+import view.ATM;
+import view.LoginView;
+
+public class ViewManager {
+	
+	private Container views;				// the collection of all views in the application
+	private Database db;					// a reference to the database
+	public BankAccount account;			// the user's bank account
+	private BankAccount destination;		// an account to which the user can transfer funds
+	
+	/**
+	 * Constructs an instance (or object) of the ViewManager class.
+	 * 
+	 * @param layout
+	 * @param container
+	 */
+	
+	public ViewManager(Container views) {
+		this.views = views;
+		this.db = new Database();
+	}
+	
+	///////////////////// INSTANCE METHODS ////////////////////////////////////////////
+	
+	/**
+	 * Routes a login request from the LoginView to the Database.
+	 * 
+	 * @param accountNumber
+	 * @param pin
+	 */
+	
+	public void login(String accountNumber, char[] pin) {
+		String userPin = new String(pin);
+		
+		if (accountNumber != null && userPin != null && accountNumber.length() > 0 && userPin.length() > 0) {
+			account = db.getAccount(Long.valueOf(accountNumber), Integer.valueOf(new String(pin)));
+			
+			if (account == null) {
+				LoginView lv = ((LoginView) views.getComponents()[ATM.LOGIN_VIEW_INDEX]);
+				lv.updateErrorMessage("Invalid account number and/or PIN.");
+			} else {
+				switchTo(ATM.HOME_VIEW);
+				
+				LoginView lv = ((LoginView) views.getComponents()[ATM.LOGIN_VIEW_INDEX]);
+				lv.updateErrorMessage("");
+			}
+		}
+	}
+	
+	/**
+	 * Switches the active (or visible) view upon request.
+	 * 
+	 * @param view
+	 */
+	
+	public void switchTo(String view) {
+		((CardLayout) views.getLayout()).show(views, view);
+	}
+	
+	/**
+	 * Routes a shutdown request to the database before exiting the application. This
+	 * allows the database to clean up any open resources it used.
+	 */
+	public void CreateAccount(User user) {
+		BankAccount account = new BankAccount('Y', db.getMaxAccountNumber()+1, 0.00, user);
+		db.insertAccount(account);
+	}
+	public void shutdown() {
+		try {			
+			int choice = JOptionPane.showConfirmDialog(
+				views,
+				"Are you sure?",
+				"Shutdown ATM",
+				JOptionPane.YES_NO_OPTION,
+				JOptionPane.QUESTION_MESSAGE
+			);
+			
+			if (choice == 0) {
+				db.shutdown();
+				System.exit(0);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
